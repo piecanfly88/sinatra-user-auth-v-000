02bb6092042573f8c1b91d50e86bcada618ab881
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-11-06 19:47:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,8 @@ class ApplicationController < Sinatra::Base
     @user.save
     session[:user_id] = @user.id
 
-    redirect '/users/home'
+    redirect '/users/home' 
+    puts params
   end
 
   get '/sessions/login' do
